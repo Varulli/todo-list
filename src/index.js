4c7d6d852a54a6387b70e52dc9aa9781a42a8d61
Varulli <120419874+Varulli@users.noreply.github.com>
2023-07-24 14:46:44 -0400

    Create DOM controller

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
 import { TodoItem, TodoProject } from './todoObjects.js';
 
-const ProjectsController = (() => {
-    const projects = [];
+const DOMController = (() => {
 
-    const getProjects = () => projects;
 
-    const addProject = (title) => {
-        projects.push(TodoProject(title));
-        console.log(projects);
-    }
-    const removeProject = (index) => {
-        projects.splice(index, 1);
-        console.log(projects);
-    }
-    const addTodoToProject = (title, description, dueDate, priority, notes, index) => {
-        getProjects()[index].addTodo(title, description, dueDate, priority, notes);
-    }
-    const removeTodoFromProject = (projectIndex, todoIndex) => {
-        getProjects()[projectIndex].removeTodo(todoIndex);
-    }
-    const updateTodoOfProject = (title, description, dueDate, priority, notes, projectIndex, todoIndex) => {
-        removeTodoFromProject(projectIndex, todoIndex);
-        addTodoToProject(title, description, dueDate, priority, notes, projectIndex);
-    }
-    const toggleSortModeOfProject = (index) => {
-        getProjects()[index].toggleSortMode();
-    }
 
-    return { getProjects, addProject, removeProject, addTodoToProject, removeTodoFromProject, updateTodoOfProject, toggleSortModeOfProject };
+    const ProjectsController = (() => {
+        const projects = [];
+
+        const getProjects = () => projects;
+
+        const addProject = (title) => {
+            projects.push(TodoProject(title));
+            console.log(projects);
+        }
+        const removeProject = (index) => {
+            projects.splice(index, 1);
+            console.log(projects);
+        }
+        const addTodoToProject = (title, description, dueDate, priority, notes, index) => {
+            getProjects()[index].addTodo(title, description, dueDate, priority, notes);
+        }
+        const removeTodoFromProject = (projectIndex, todoIndex) => {
+            getProjects()[projectIndex].removeTodo(todoIndex);
+        }
+        const updateTodoOfProject = (title, description, dueDate, priority, notes, projectIndex, todoIndex) => {
+            removeTodoFromProject(projectIndex, todoIndex);
+            addTodoToProject(title, description, dueDate, priority, notes, projectIndex);
+        }
+        const toggleSortModeOfProject = (index) => {
+            getProjects()[index].toggleSortMode();
+        }
+
+        return { getProjects, addProject, removeProject, addTodoToProject, removeTodoFromProject, updateTodoOfProject, toggleSortModeOfProject };
+    })();
+
 })();
