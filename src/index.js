ae5581f956271c164a6e92f6b4a471633e374ce2
Varulli <120419874+Varulli@users.noreply.github.com>
2023-07-24 14:07:10 -0400

    Fix import bugs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import { TodoItem, TodoProject } from './todoObjects.js';
 const ProjectsController = (() => {
     const projects = [];
 
+    const getProjects = () => projects;
+
     const addProject = (title) => {
         projects.push(TodoProject(title));
+        console.log(projects);
+    }
+    const removeProject = (index) => {
+        projects.splice(index, 1);
+        console.log(projects);
     }
 
-    return { addProject };
+    return { addProject, removeProject, getProjects };
 })();
--- src/todoObjects.js
+++ src/todoObjects.js
@@ -4,26 +4,56 @@ function TodoItem(title, description, dueDate, priority, notes) {
     const getDueDate = () => dueDate;
     const getPriority = () => priority;
     const getNotes = () => notes;
-    return { getTitle, getDescription, getDueDate, getPriority, getNotes };
+
+    const setTitle = (newTitle) => title = newTitle;
+    const setDescription = (newDescription) => description = newDescription;
+    const setDueDate = (newDueDate) => dueDate = newDueDate;
+    const setPriority = (newPriority) => priority = newPriority;
+    const setNotes = (newNotes) => notes = newNotes;
+
+    return {
+        getTitle, getDescription, getDueDate, getPriority, getNotes,
+        setTitle, setDescription, setDueDate, setPriority, setNotes
+    };
 }
 
 function TodoProject(title) {
     const todos = [];
     let sortMode = 'dueDate';
 
-    const addTask = (title, description, dueDate, priority, notes) => {
-        todos.push(TodoItem(title, description, dueDate, priority, notes));
-    }
-    const removeTask = (index) => {
-        todos.splice(index, 1);
-    }
-    const toggleSortMode = () => {
-        sortMode = sortMode === 'dueDate' ? 'priority' : 'dueDate';
-    }
-
     const getTitle = () => title;
     const getTodos = () => todos;
     const getSortMode = () => sortMode;
 
-    return { addTask, removeTask, toggleSortMode, getTitle, getTodos, getSortMode };
+    const setTitle = (newTitle) => title = newTitle;
+    const toggleSortMode = () => {
+        sortMode = sortMode === 'dueDate' ? 'priority' : 'dueDate';
+        sortTodos();
+    }
+
+    const sortTodos = () => {
+        if (sortMode === 'dueDate')
+            todos.sort((a, b) => {
+                if (a.getDueDate() < b.getDueDate()) return -1;
+                else if (a.getDueDate() > b.getDueDate()) return 1;
+                else if (a.getDueDate() === b.getDueDate()) return 0;
+            });
+        else if (sortMode === 'priority')
+            todos.sort((a, b) => {
+                if (a.getPriority() < b.getPriority()) return -1;
+                else if (a.getPriority() > b.getPriority()) return 1;
+                else if (a.getPriority() === b.getPriority()) return 0;
+            });
+    }
+    const addTodo = (title, description, dueDate, priority, notes) => {
+        todos.push(TodoItem(title, description, dueDate, priority, notes));
+        sortTodos();
+    }
+    const removeTodo = (index) => {
+        todos.splice(index, 1);
+    }
+
+    return { setTitle, addTodo, removeTodo, toggleSortMode, getTitle, getTodos, getSortMode };
 }
+
+export { TodoItem, TodoProject };
