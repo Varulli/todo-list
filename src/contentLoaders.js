2f843db02de45677994f5f00096c3e965d8dc87a
Varulli <120419874+Varulli@users.noreply.github.com>
2023-07-24 15:40:29 -0400

    Create 'view all projects' loading function

--- src/contentLoaders.js
+++ src/contentLoaders.js
@@ -6,7 +6,46 @@ function clearContent() {
 }
 
 function loadProjects(projects) {
+    clearContent();
 
+    const projectView = document.createElement('div');
+    projectView.classList.add('project-view');
+
+    for (const project of projects) {
+        const projectCard = document.createElement('button');
+        projectCard.classList.add('project-card');
+
+        const projectTitle = document.createElement('div');
+        projectTitle.classList.add('project-title');
+        projectTitle.textContent = project.getTitle();
+
+        const projectDescription = document.createElement('div');
+        projectDescription.classList.add('project-description');
+        projectDescription.textContent = project.getDescription();
+
+        projectCard.appendChild(projectTitle);
+        projectCard.appendChild(projectDescription);
+
+        const todos = project.getTodos();
+        if (todos.length > 0) {
+            const preview = document.createElement('ol');
+            preview.classList.add('preview');
+
+            const limit = todos.length > 3 ? 3 : todos.length;
+            for (let i = 0; i < limit; i++) {
+                const previewItem = document.createElement('li');
+                previewItem.classList.add('preview-item');
+                previewItem.textContent = todos[i].getTitle();
+                preview.appendChild(previewItem);
+            }
+
+            projectCard.appendChild(preview);
+        }
+
+        projectView.appendChild(projectCard);
+    }
+
+    content.appendChild(projectView);
 }
 
 function loadProject(projects, index) {
