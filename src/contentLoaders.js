dd0144d250a4cda10d31165e17e5f9104c0ab827
Varulli <120419874+Varulli@users.noreply.github.com>
2023-07-24 15:07:44 -0400

    Create function to clear content div

--- src/contentLoaders.js
+++ src/contentLoaders.js
@@ -1,9 +1,18 @@
-function loadProjects() {
+const content = document.querySelector('.content');
+
+function clearContent() {
+    while (content.hasChildNodes())
+        content.removeChild(content.firstChild);
+}
+
+function loadProjects(projects) {
+    for (const project of projects) {
+        const projectWrapper
+    }
+}
+
+function loadProject(projects, index) {
 
 }
 
-function loadProject(index) {
-
-}
-
-export { loadProjects, loadProject };
+export { clearContent, loadProjects, loadProject };
